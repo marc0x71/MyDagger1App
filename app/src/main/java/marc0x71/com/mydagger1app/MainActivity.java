85213a2ca123900c9d1afbd272e9756c210704dc
marc0x71 <p1pp0n3.1>
2016-03-03 13:17:31 +0100

    Initial import

---
+++ app/src/main/java/marc0x71/com/mydagger1app/MainActivity.java
@@ -0,0 +1,13 @@
+package marc0x71.com.mydagger1app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/marc0x71/com/mydagger1app/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package marc0x71.com.mydagger1app;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
